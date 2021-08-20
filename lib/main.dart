064812c5f7f080571f97cce2b620abdf09348bcc
IRizerX <50023287+IRizerX@users.noreply.github.com>
2021-08-20 22:18:35 +0300

    copyright added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:residencies/pages/homepage.dart';
 
+// # Clone Done By IRizerX
+// # Instagram : https://www.instagram.com/irizerx/
+// # GitHub : https://github.com/IRizerX/FindResidenciesClone
+
 void main() {
   runApp(MyApp());
 }
